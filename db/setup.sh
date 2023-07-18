a6fd86950ba308d25e53a47f12ab1606cdfd6805
Chad-Mowbray <mowbray.chad@gmail.com>
2023-07-18 17:23:49 -0400

    remove port forwarding

--- db/setup.sh
+++ db/setup.sh
@@ -1,2 +1,2 @@
 docker build -t people_db ./db
-docker run --name pg_db --rm -e POSTGRES_PASSWORD=password -p 5444:5432 -d people_db
+docker run --name pg_db --rm -e POSTGRES_PASSWORD=password -d people_db
