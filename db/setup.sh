970c61a3ceb055ce5a5e1db87fcd4d935d58fdc6
Block141 <block141@gmail.com>
2024-03-23 09:09:35 -0500

    Init commit

--- db/setup.sh
+++ db/setup.sh
@@ -1,2 +1,4 @@
-docker build -t people_db ./db
-docker run --name pg_db --rm -e POSTGRES_PASSWORD=password -d people_db
+docker build -t crustacean_db .
+docker run --name pg_db --rm -e POSTGRES_PASSWORD=password -d crustacean_db
+
+# docker exec -it pg_db psql -h localhost -p 5432 -U postgres -d crustacean
