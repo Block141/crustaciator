2e0fa0304d071931bb4c02cfe33274567c8be026
Block141 <block141@gmail.com>
2024-03-23 11:08:00 -0500

    Changes to network ports

--- db/setup.sh
+++ db/setup.sh
@@ -1,5 +1,5 @@
 docker build -t crustacean_db ./db
-docker run --name pg_db --rm -e POSTGRES_PASSWORD=password -d crustacean_db
+docker run --name pg_db --rm -p 5444:5432 -e POSTGRES_PASSWORD=password -d crustacean_db
 
 #Local testing.
 # docker exec -it pg_db psql -h localhost -p 5432 -U postgres -d crustacean
